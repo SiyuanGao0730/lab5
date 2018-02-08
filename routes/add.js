a2302f16acd6ed2ae1030f062aafbb898914d138
Siyuan Gao <sig019@ucsd.edu>
2018-02-08 00:12:19 -0800

    lab5

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,16 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	var newName = req.query.name;
+	var newDes = req.query.description;
+	var newOne = {
+		"name": newName,
+		"description": newDes,
+		"imageURL": "http://lorempixel.com/500/500/people"
+	};
+
+	data.friends.push(newOne);
+	
+	console.log(newOne);
+	res.render('index', data);
  }
